65c7ec0543092a035e73387c307193852e4fe1bb
Nathan Roberts <38477031+NRobot@users.noreply.github.com>
2020-05-28 17:23:53 -0500

    Added vowel replacement method

--- src/com/natecode/ProcessWord.java
+++ src/com/natecode/ProcessWord.java
@@ -1,20 +1,19 @@
 package com.natecode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ProcessWord {
-    private final String word;
+    private final String wordEntered;
     private final int wordLength;
     List<Object> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'y');
-    private final List<String> syllables = new ArrayList<String>();
+    private final List<String> vowelsFromWord = new ArrayList<>();
 
     public ProcessWord(String word) {
-        this.word = word;
+        this.wordEntered = word;
         wordLength = word.length();
         findSyllables(word);
-        printResults(syllables);
+        printResults(vowelsFromWord);
+        newVowels(vowelsFromWord);
     }
 
     private void findSyllables(String word) {
@@ -32,22 +31,63 @@ public class ProcessWord {
 
                 if  (i != (wordLength - 1) && !vowels.contains(word.charAt(i+1))) {
                     newSyllable = syllableString.toString();
-                    syllables.add((syllableCount - 1), newSyllable);
+                    vowelsFromWord.add((syllableCount - 1), newSyllable);
                     syllableString.setLength(0);
                 }
                 if  (i == (wordLength - 1)) {
                     newSyllable = syllableString.toString();
-                    syllables.add((syllableCount - 1), newSyllable);
+                    vowelsFromWord.add((syllableCount - 1), newSyllable);
                     syllableString.setLength(0);
                 }
             }
         }
     }
 
+    private void newVowels(List listOfVowelStrings) {
+            Map<String, Object> newVowelGroups = new HashMap<String, Object>();
+
+        // Initial set of alternatives
+        newVowelGroups.put("u", "oo");
+        newVowelGroups.put("oo", "u");
+        newVowelGroups.put("e", "i");
+        newVowelGroups.put("ea", "ee");
+        newVowelGroups.put("i", "u");
+
+        // Numbers
+        newVowelGroups.put("o", "0");
+        // mapOfOptions.put("i", "1");
+        newVowelGroups.put("z", "2");
+        newVowelGroups.put("s", "$");
+        newVowelGroups.put("h", "#");
+        newVowelGroups.put("a", "@");
+        newVowelGroups.put("e", "");
+
+        if(listOfVowelStrings !=null) {
+            String newWordSpelling = "If you are reading this, something went wrong";
+            String oldSubString;
+            String newSubString;
+
+            for(Object syllable : listOfVowelStrings) {
+
+                for (Map.Entry<String, Object> entry :newVowelGroups.entrySet()) {
+                    if(syllable.toString().equals(entry.getKey())) {
+                        oldSubString = syllable.toString();
+                        newSubString = entry.getValue().toString();
+                        newWordSpelling = wordEntered.replace(oldSubString, newSubString);
+                        System.out.println("New version of word: " + newWordSpelling);
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
     private void printResults(List listOfSyllables) {
         int counter = 0;
         int numberOfSyllables = listOfSyllables.size();
-        System.out.println("The word: " + word);
+        System.out.println("The word: " + wordEntered);
         System.out.println("Number of syllables: " + numberOfSyllables);
         if(listOfSyllables !=null) {
             for(Object syllablePrinted : listOfSyllables) {
